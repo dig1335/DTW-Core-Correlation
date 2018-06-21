2fd7e2dbff189c1221b080288b7870da3d25fc82
Dai Grady <39172430+dig1335@users.noreply.github.com>
2018-06-21 17:19:33 +0200

    Add files via upload

--- Subsequence DTW BA.R
+++ Subsequence DTW BA.R
@@ -6,8 +6,14 @@ Full10Ti <- read.csv("Full10Test.csv")
 
 ## TiD represents Ti normalised but multiplied by 10000 to easier visualise
 
-alignmentOBE <- dtw(Ti10A$TiD, Ti9B$TiD, keep=TRUE,step=asymmetric, open.end=TRUE,open.begin=TRUE)
-plot(alignmentOBE,type="two",off=4000);
+alignmentOBE <- dtw(Ti10A$TiD, Ti9B$TiD, 
+                    keep=TRUE,step=asymmetric, 
+                    open.end=TRUE,
+                    open.begin=TRUE)
+
+##Plot both curves - reference then query below
+
+plot(alignmentOBE,type="two",off=3500);
 
 ## Triple plot to see both plots easier with the links between 
 
@@ -15,7 +21,11 @@ hq <- (0:200)/15
 hq <- round(hq*1000)      #  indices in query for  pi/4 .. 7/4 pi
 hw <- (alignmentOBE$index1 %in% hq)   # where are they on the w. curve?
 hi <- (1:length(alignmentOBE$index1))[hw];   # get the indices of TRUE elems
-dtwPlotThreeWay(alignmentOBE,match.indices=hi, ylab="9B\n", xlab="\n10A", margin = 3.75, inner.margin = 0.44, main = "Ti cps: 9B vs. 10A");
+dtwPlotThreeWay(alignmentOBE,
+                match.indices=hi, 
+                ylab="9B (base of composite)\n", xlab="\n10A (missing section #1)", 
+                margin = 3.75, inner.margin = 0.44, 
+                main = "Ti cps: 9B vs. 10A")
 
 ##Full 10 plot - working on linking then to floating below (section 11)
 
@@ -23,7 +33,7 @@ alignment2 <- dtw(Full10Ti$TiD, Ti9B$TiD, keep=TRUE,step=asymmetric, open.end=TR
 plot(alignment2,type="two",off=4000);
 
 
-hq <- (0:200)/5
+hq <- (0:200)/10
 hq <- round(hq*1000)      #  indices in query for  pi/4 .. 7/4 pi
 hw <- (alignment2$index1 %in% hq)   # where are they on the w. curve?
 hi <- (1:length(alignment2$index1))[hw];   # get the indices of TRUE elems
